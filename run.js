54f0a72e168886e5b12bf1c4bcfda14a53bcdbe3
vestrial <sandra.sieroux@googlemail.com>
2016-02-20 17:04:30 +0100

    collected all tables before parse

--- run.js
+++ run.js
@@ -1,65 +1,106 @@
 var Mustache = require("mustache");
+var tableCollection = collectTables();
 
-var runPattern = "Die Runner gehen zu einem Treffen in {{location}} für ihren nächsten Job." +
+var runPattern = "Die Runner gehen zu einem Treffen in {{meetLocation}} für ihren nächsten Job." +
 " Sie werden von {{employer}} für ein/e/n {{jobType}} gegen {{macGuffin}} angeheuert." +
 " Der Run wird kompliziert, als {{twist}}!"
 
 console.log(parse(runPattern));
 
+function collectTables() {
+  var tableCollection = [];
+  tableCollection.push(meetLocation());
+  tableCollection.push(nightlife());
+  tableCollection.push(employer());
+  tableCollection.push(jobType());
+  tableCollection.push(macGuffin());
+  tableCollection.push(twist());
+  return tableCollection;
+}
+
 function parse(pattern) {
-  var tables = {
-    nightlife: pickFrom(nightlife()),
-    location: pickFrom(location()),
-    employer: employer(),
-    jobType: jobType(),
-    macGuffin: macGuffin(),
-    twist: twist()
+  var templateMap = {};
+  for(i = 0; i < tableCollection.length; i++) {
+    var table = tableCollection[i];
+    addMustacheTemplate(templateMap, table);
   }
   while(pattern.indexOf('{{') >= 0) {
-    pattern = Mustache.render(pattern, tables);
+    pattern = Mustache.render(pattern, templateMap);
   }
   return pattern;
 }
 
-function location() {
-  return locations = {
-    1: "{{nightlife}}",
-    2: "Lagerhaus, Dock, Fabrik oder anderer wenig frequentierter Ort",
-    3: "Barrens oder irgendein anderes urbanes Dreksloch",
-    4: "Fahrendes Fahrzeug",
-    5: "Matrixhost",
-    6: "Astralraum"
+function addMustacheTemplate(templateMap, table) {
+  templateMap[table.name] = function() {
+    return pickFrom(table);
+  }
+}
+
+function pickFrom(randomContent) {
+  var numerOfOptions = Object.keys(randomContent.table).length;
+  var choice = rollDie(numerOfOptions)
+  return randomContent.table[choice];
+}
+
+function meetLocation() {
+  return {
+    name: "meetLocation",
+    table: {
+      1: "{{nightlife}}",
+      2: "Lagerhaus, Dock, Fabrik oder anderer wenig frequentierter Ort",
+      3: "Barrens oder irgendein anderes urbanes Dreksloch",
+      4: "Fahrendes Fahrzeug",
+      5: "Matrixhost",
+      6: "Astralraum"
+    }
   }
 }
 
 function nightlife() {
-  return locations = {
-    1: "einer Bar",
-    2: "einem Club",
-    3: "einem Restaurant"
+  return {
+    name: "nightlife",
+    table: {
+      1: "einer Bar",
+      2: "einem Club",
+      3: "einem Restaurant"
+    }
   }
 }
 
-function pickFrom(randomMap) {
-  var numerOfOptions = Object.keys(randomMap).length;
-  var choice = rollDie(numerOfOptions)
-  return randomMap[choice];
-}
-
 function employer() {
-  return "employer";
+  return {
+    name: "employer",
+    table:  {
+      1: "employer"
+    }
+  }
 }
 
 function jobType() {
-  return "jobType";
+  return {
+    name: "jobType",
+    table:  {
+      1: "jobType"
+    }
+  }
 }
 
 function macGuffin() {
-  return "macGuffin";
+  return {
+    name: "macGuffin",
+    table:  {
+      1: "macGuffin"
+    }
+  }
 }
 
 function twist() {
-  return "twist";
+  return {
+    name: "twist",
+    table:  {
+      1: "twist"
+    }
+  }
 }
 
 function rollD6() {
