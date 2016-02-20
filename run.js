fbc4470196b9858955e196c147f0f63bebcfd8a6
vestrial <sandra.sieroux@googlemail.com>
2016-02-20 14:54:00 +0100

    prepares pattern mechanism

--- run.js
+++ run.js
@@ -1,19 +1,30 @@
 
 
-console.log("Die Runner gehen zu einem Treffen in " + location() + " für ihren nächsten Job." +
+var runPattern = "Die Runner gehen zu einem Treffen in " + pickFrom(location()) + " für ihren nächsten Job." +
 " Sie werden von " + employer() + " für ein/e/n " + jobType() + " gegen " + macGuffin() +
-" angeheuert. Der Run wird kompliziert, als " + twist() + "!");
+" angeheuert. Der Run wird kompliziert, als " + twist() + "!"
+
+console.log(parse(runPattern));
+
+function parse(pattern) {
+  return pattern;
+}
 
 function location() {
-  locations = {
+  return locations = {
     1: "Bar, Club oder Restaurant",
     2: "Lagerhaus, Dock, Fabrik oder anderer wenig frequentierter Ort",
-    3: "Barrens oder irgendein anderes urbanes Drekloch",
+    3: "Barrens oder irgendein anderes urbanes Dreksloch",
     4: "Fahrendes Fahrzeug",
     5: "Matrixhost",
     6: "Astralraum"
   }
-  return locations[rollD6()];
+}
+
+function pickFrom(randomMap) {
+  var numerOfOptions = Object.keys(randomMap).length;
+  var choice = rollDie(numerOfOptions)
+  return randomMap[choice];
 }
 
 function employer() {
@@ -33,5 +44,9 @@ function twist() {
 }
 
 function rollD6() {
-  return Math.floor((Math.random() * 6) + 1);
+  return rollDie(6);
+}
+
+function rollDie(sides) {
+  return Math.floor((Math.random() * sides) + 1);
 }
