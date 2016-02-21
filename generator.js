9eda9cfdebdf0b71ab32359fcbeb1b1fb124da54
vestrial <sandra.sieroux@googlemail.com>
2016-02-21 11:16:26 +0100

    proper escaping for mustache

--- generator.js
+++ generator.js
@@ -3,9 +3,9 @@ var randomizer = require("./randomizer");
 module.exports.randomRun = randomRun
 
 function randomRun() {
-  var runPattern = "Die Runner gehen zu einem Treffen in {{meetLocation}} für ihren nächsten Job." +
-  " Sie werden von {{employer}} für ein/e/n {{jobType}} gegen {{macGuffin}} angeheuert." +
-  " Der Run wird kompliziert, denn {{twist}}!"
+  var runPattern = "Die Runner gehen zu einem Treffen in {{{meetLocation}}} für ihren nächsten Job." +
+  " Sie werden von {{{employer}}} für ein/e/n {{{jobType}}} gegen {{{macGuffin}}} angeheuert." +
+  " Der Run wird kompliziert, denn {{{twist}}}!"
 
   return randomizer.fill(runPattern);
 }
